1817351491992c1435c837117f73b47bc0f592df
wnyl_xx <hyy96hyy@naver.com>
2025-03-19 18:01:51 +0900

    push용 커밋

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.get("/data", async (req, res) => {
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`서버 실행중: http://localhost: ${PORT}`);
+    console.log(`서버 실행중: http://localhost:${PORT}`);
 });
